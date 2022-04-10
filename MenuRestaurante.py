2c3bee3c1db1ee7f6f8221fbcacb7af347413d63
Renato Barbosa <98436200+ORenatoBarbosa@users.noreply.github.com>
2022-04-09 23:14:35 -0300

    Create MenuRestaurante.py

---
+++ MenuRestaurante.py
@@ -0,0 +1,68 @@
+# cardapio digital de um Restaurante
+cod = int(input('Escolha uma das opções do Cardápio, Código(100 a 120):'))
+quant = int(input('Quantidade:'))
+if cod == 100:
+    print('Cachorro Quente')
+    print(f'Preço: R${(quant * 15.2):.2f}')
+elif cod == 101:
+    print('Bauru Simples')
+    print(f'Preço: R${(quant * 10.3):.2f}')
+elif cod == 102:
+    print('Bauru com Ovo.')
+    print(f'Preço: R${(quant * 12.5):.2f}')
+elif cod == 103:
+    print('Hamburguer.')
+    print(f'Preço: R${(quant * 14.2):.2f}')
+elif cod == 104:
+    print('Cheesburguer.')
+    print(f'Preço: R${(quant * 20.7):.2f}')
+elif cod == 105:
+    print('Suco.')
+    print(f'Preço: R${(quant * 7.2):.2f}')
+elif cod == 106:
+    print('Refrigerante.')
+    print(f'Preço: R${(quant * 5):.2f}')
+elif cod == 107:
+    print('Ala minuta.')
+    print(f'Preço: R${(quant * 22.8):.2f}')
+elif cod == 108:
+    print('Buffet Livre')
+    print(f'Preço: R${(quant * 25.0):.2f}')
+elif cod == 109:
+    print('Cerveja 600ml')
+    print(f'Preço: R${(quant * 10.0):.2f}')
+elif cod == 110:
+    print('Cerveja Litrão')
+    print(f'Preço: R${(quant * 12.0):.2f}')
+elif cod == 111:
+    print('Picadão completo')
+    print(f'Preço: R${(quant * 50.0):.2f}')
+elif cod == 112:
+    print('Picadão simples')
+    print(f'Preço: R${(quant * 25.0):.2f}')
+elif cod == 113:
+    print('Batata Frita Média')
+    print(f'Preço: R${(quant * 20.0):.2f}')
+elif cod == 114:
+    print('Picanha na Tábua')
+    print(f'Preço: R${(quant * 75.0):.2f}')
+elif cod == 115:
+    print('Bacalhau Assado')
+    print(f'Preço: R${(quant * 95.0):.2f}')
+elif cod == 116:
+    print('Filé a Parmegiana')
+    print(f'Preço: R${(quant * 63.0):.2f}')
+elif cod == 117:
+    print('Frango a Passarinho c/ especiarias')
+    print(f'Preço: R${(quant * 47.0):.2f}')
+elif cod == 118:
+    print('Filé de Tilápia a milanesa')
+    print(f'Preço: R${(quant * 50.0):.2f}')
+elif cod == 119:
+    print('Massa molho branco c/ Brócolis')
+    print(f'Preço: R${(quant * 60.0):.2f}')
+elif cod == 120:
+    print('Galeto com massas c/ Vinho gran Reserva da Serra Gaucha')
+    print(f'Preço: R${(quant *155.0):.2f}')
+
+# Renato Barbosa
